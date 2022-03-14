f2a93efd5655a6c46d0e03ef790bc5fa66811cb8
Lucas Dias <lucas.dias@mindera.com>
2022-03-14 18:22:39 +0000

    chore: Initial tweaks to create-react-app defaults

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styled from 'styled-components';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CustomBtn>Press me</CustomBtn>
     </div>
   );
 }
 
 export default App;
+
+const CustomBtn = styled.button`
+  background-color: red;
+`
